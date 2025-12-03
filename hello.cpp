4927a7c041e81b0dfae80e2ba5451558884ec298
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-03 14:55:56 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"hello git hub - by chiru";
+   cin.ignore();
+   cin.get(); 
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<"sum is"<<a+b<<endl;
+    cin.ignore();
+    cin.get();
+    return 0;
+}
