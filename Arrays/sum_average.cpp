c837d1c4e2c6448f2a390d0e528b0fb86c6dcad7
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-04 10:08:27 +0530

    Add comments for clarity in array programs

--- Arrays/sum_average.cpp
+++ Arrays/sum_average.cpp
@@ -6,19 +6,28 @@ int main()
     int arr[20],n,i,sum=0;
     double avg;
     cout<<"Enter number of elements in array"<<endl;
-    cin>>n;
+    cin>>n;        // Input number of elements in array
     cout<<"enter "<<n<<" elements"<<endl;
     for (i=0;i<n;i++)
-    cin>>arr[i];
+    cin>>arr[i];       // Taking input for all array elements
     for(i=0;i<n;i++)
-    sum=sum+arr[i];
-    avg=sum/n;
-    cout<<"Sum is:"<<sum<<endl;
+    sum=sum+arr[i];       // Calculating the total sum of elements
+    avg=sum/n;         // Calculating average
+    cout<<"Sum is:"<<sum<<endl;         // Displaying the results
     cout<<"average is:"<<avg<<endl;
     cin.ignore();
     cin.get();
     return 0;
 }
+
+
+
+
+
+
+
+
+
 
     
                     
