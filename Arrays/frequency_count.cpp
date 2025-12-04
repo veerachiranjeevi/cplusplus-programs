62305e5021778b6f0ec24308497f3b0c2f6e5a1d
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-04 11:13:55 +0530

    Add comments for clarity in frequency program

--- Arrays/frequency_count.cpp
+++ Arrays/frequency_count.cpp
@@ -5,24 +5,36 @@ int main(){
     int arr[30],key,n,i,count=0;
     bool flag=true;
     cout<<"Enter how many elements"<<endl;
-    cin>>n;
+    cin>>n;        // Input number of elements
     cout<<"Enter an array"<<endl;
     for (i=0;i<n;i++)
-    cin>>arr[i];
+    cin>>arr[i];        // Taking array elements input
     cout<<"Enter key value"<<endl;
-    cin>>key;
+    cin>>key;     // Input the key value to search its frequency
     for (i=0;i<n;i++)
-    if (key==arr[i])
+    if (key==arr[i])   // Searching for key and counting occurrences
     {
     count=count+1;
     flag=false;
     }
     if (flag)
-    cout<<"key value "<<key<<" is not present"<<endl;
+    cout<<"key value "<<key<<" is not present"<<endl;   // If key is not found
     else
-    cout<<"Frequency of key is:"<<count<<endl;
+    cout<<"Frequency of key is:"<<count<<endl;   // If key is found, display frequency
     cin.ignore();
     cin.get();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
    
