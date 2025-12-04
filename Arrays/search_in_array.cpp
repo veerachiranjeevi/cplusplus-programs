bcdb7ddfd0771927d9270c37595b92b09fb3d9d7
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-04 10:53:07 +0530

    Add comments for clarity in search program

--- Arrays/search_in_array.cpp
+++ Arrays/search_in_array.cpp
@@ -5,20 +5,28 @@ int main(){
     int arr[30],key,n,i;
     bool flag=true;
     cout<<"Enter how many elements"<<endl;
-    cin>>n;
+    cin>>n;        // Input number of elements
     cout<<"Enter an array"<<endl;
     for (i=0;i<n;i++)
-    cin>>arr[i];
+    cin>>arr[i];      // Taking array elements input
     cout<<"Enter key value"<<endl;
-    cin>>key;
+    cin>>key;       // Input the key value to search
     for (i=0;i<n;i++)
-    if(key==arr[i]){
-                    cout<<"key value "<<arr[i]<<" is present";
-                    flag=false;
-                    break;}
+    if(key==arr[i])            // Compare key with each element
+    {               
+        cout<<"key value "<<arr[i]<<" is present";     
+        flag=false;       // If found, print and stop searching
+        break;
+    }
     if (flag)
-    cout<<"key value "<<key<<" is not present"<<endl;
+    cout<<"key value "<<key<<" is not present"<<endl;    // If not found after loop, print not present
     cin.ignore();
     cin.get();
     return 0;
 }
+
+
+
+
+
+
