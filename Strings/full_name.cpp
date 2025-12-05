9e1f0fa27153a32f4dfa93bad73e1ce5958725fa
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-05 14:43:43 +0530

    Add full name string input program

---
+++ Strings/full_name.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your full name: " << endl;
+    getline(cin, name); // Input full name including spaces
+
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
