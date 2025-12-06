aeb4f830cb9c0505f9cbf95ceceeb7b2c74d8a2e
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-06 09:09:30 +0530

    Add basic void function program with comments

---
+++ Functions/greet_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+// Function definition
+void greet() {
+    cout << "Hello Chiru! Welcome to functions 🚀";
+}
+
+int main() {
+    greet(); // Calling the function
+    return 0;
+}
