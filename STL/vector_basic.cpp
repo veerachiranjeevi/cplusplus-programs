6b853b1ce6228ccce38d502e24b92c208cd3eb22
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-11 10:35:32 +0530

    Added basic vector initialization and traversal program

---
+++ STL/vector_basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    cout << "Vector elements: ";
+    for(int x : v) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
