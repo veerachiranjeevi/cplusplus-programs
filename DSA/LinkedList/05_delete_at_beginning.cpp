e9569fb7ed7a543850015a6029626677592d6a69
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-14 13:09:35 +0530

    Linked List: Delete node at beginning

---
+++ DSA/LinkedList/05_delete_at_beginning.cpp
@@ -0,0 +1,46 @@
+#include<iostream>  
+using namespace std;
+ 
+struct Node{
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* n1 = new Node();
+    Node* n2 = new Node();
+    Node* n3 = new Node();
+
+    n1->data = 10;
+    n2->data = 20;
+    n3->data = 30;
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
+
+    Node* head = n1;
+    Node* temp = head;
+
+    cout << "Before:" << endl;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    if (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    cout << "\nAfter deletion at beginning:" << endl;
+    temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
