07f213318d54d951f7223f23bfcfe696fb9686bb
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-15 19:33:30 +0530

    Add program 12: Reverse singly linked list using iterative method

---
+++ DSA/LinkedList/12_reverse_linked_list_iterative.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = NULL;
+    Node* curr = head;
+
+    while (curr != NULL) {
+        Node* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+Node* createList(int n) {
+    Node* head = NULL;
+    Node* tail = NULL;
+    int value;
+
+    for (int i = 0; i < n; i++) {
+        Node* newNode = new Node();
+        cout << "Enter data for node " << i + 1 << ": ";
+        cin >> value;
+
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    return head;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    Node* head = createList(n);
+    head = reverseList(head);
+
+    cout << "Reversed linked list: ";
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
