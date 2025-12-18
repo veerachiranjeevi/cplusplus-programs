599e8e56fd7d3b45e101f4f63358ce78c75af545
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-18 09:58:40 +0530

    Create basic binary tree node and print root data

---
+++ DSA/Trees/binary_tree_node_creation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+int main() {
+    Node* root = new Node();
+    root->data = 10;
+    root->left = NULL;
+    root->right = NULL;
+
+    cout << "The root data is " << root->data;
+
+    return 0;
+}
