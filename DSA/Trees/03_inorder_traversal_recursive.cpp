9274d2375146acac79f16ee4c7581213c6be5169
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-18 10:31:53 +0530

    03: Implement recursive inorder traversal for binary tree

---
+++ DSA/Trees/03_inorder_traversal_recursive.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+void inorder(Node* node) {
+    if (node == NULL)
+        return;
+
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+}
+
+int main() {
+    Node* root = new Node();
+    Node* leftChild = new Node();
+    Node* rightChild = new Node();
+
+    leftChild->data = 20;
+    leftChild->left = NULL;
+    leftChild->right = NULL;
+
+    rightChild->data = 30;
+    rightChild->left = NULL;
+    rightChild->right = NULL;
+
+    root->data = 10;
+    root->left = leftChild;
+    root->right = rightChild;
+
+    inorder(root);
+
+    return 0;
+}
