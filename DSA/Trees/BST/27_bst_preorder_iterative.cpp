478aecae5b86d2c701b4d1bb6221554da4237bf3
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-23 18:43:45 +0530

    27: Iterative preorder traversal of BST using stack

---
+++ DSA/Trees/BST/27_bst_preorder_iterative.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* insertBST(Node* node, int value) {
+    if (node == NULL) {
+        Node* temp = new Node();
+        temp->data = value;
+        temp->left = NULL;
+        temp->right = NULL;
+        return temp;
+    }
+
+    if (value < node->data)
+        node->left = insertBST(node->left, value);
+    else if (value > node->data)
+        node->right = insertBST(node->right, value);
+
+    return node;
+}
+
+void preorderIterative(Node* node) {
+    if (node == NULL)
+        return;
+
+    stack<Node*> st;
+    st.push(node);
+
+    while (!st.empty()) {
+        Node* current = st.top();
+        st.pop();
+
+        cout << current->data << " ";
+
+        if (current->right != NULL)
+            st.push(current->right);
+
+        if (current->left != NULL)
+            st.push(current->left);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insertBST(root, 40);
+    root = insertBST(root, 20);
+    root = insertBST(root, 60);
+    root = insertBST(root, 10);
+    root = insertBST(root, 30);
+
+    preorderIterative(root);
+
+    return 0;
+}
