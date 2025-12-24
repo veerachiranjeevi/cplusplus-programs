4bb8a037d162697f73e9854fe64b3d04877d2d9c
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-24 22:00:45 +0530

    30: Insert element into Max Heap using heapify-up

---
+++ DSA/Heaps/01_max_heap_insertion.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void insertMaxHeap(int heap[], int& size, int value)
+{
+    heap[size] = value;
+    int current = size;
+    size++;
+
+    while (current > 0)
+    {
+        int parent = (current - 1) / 2;
+
+        if (heap[parent] < heap[current])
+        {
+            int temp = heap[parent];
+            heap[parent] = heap[current];
+            heap[current] = temp;
+
+            current = parent;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int heap[50];
+    int size = 0;
+
+    insertMaxHeap(heap, size, 50);
+    insertMaxHeap(heap, size, 30);
+    insertMaxHeap(heap, size, 40);
+    insertMaxHeap(heap, size, 10);
+    insertMaxHeap(heap, size, 60);
+
+    cout << "Max Heap elements: ";
+    for (int i = 0; i < size; i++)
+        cout << heap[i] << " ";
+
+    return 0;
+}
