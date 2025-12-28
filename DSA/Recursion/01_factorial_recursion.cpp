28e913ec679bcf91d40cceac1ded20710a248a84
Veera Chiranjeevi <veerachiranjeevi45@gmail.com>
2025-12-28 19:43:09 +0530

    01: Factorial of a number using recursion

---
+++ DSA/Recursion/01_factorial_recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+
+    return n * fact(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " is: " << fact(n);
+    return 0;
+}
